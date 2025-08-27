28f2dbea998bccc3729c19ac246407ef5b432240
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2025-08-27 14:04:23 +0800

    New Post: my-new-blog 以及，改没改完的设置...

--- src/config.ts
+++ src/config.ts
@@ -55,8 +55,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Yukari0201",
+	bio: "喵呜～",
 	links: [
 		{
 			name: "Twitter",
---
+++ src/content/posts/my-new-blog.md
@@ -0,0 +1,15 @@
+---
+title: 我的新博客！
+published: 2025-08-27
+description: This is the first post of my new Astro blog.
+tags:
+  - Hello World
+category: Hello World
+draft: false
+---
+
+昨天和今天，我将我的博客从 Hexo + NexT 迁移到 Astro + Fuwari.
+
+之后会慢慢将文章也都迁移过来。
+
+各位尽请期待啦！
