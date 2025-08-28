33f86a8a10462fe50dd44a0d8f9ba35e7339145d
Yukari0201 <130890393+Yukari0201@users.noreply.github.com>
2025-08-28 17:48:59 +0800

    Try to fix GitHub Actions Check Error.

--- src/i18n/languages/es.ts
+++ src/i18n/languages/es.ts
@@ -5,6 +5,7 @@ export const es: Translation = {
 	[Key.home]: "Inicio",
 	[Key.about]: "Sobre mí",
 	[Key.archive]: "Archivo",
+	[Key.links]: "Links",
 	[Key.search]: "Buscar",
 
 	[Key.tags]: "Etiquetas",
